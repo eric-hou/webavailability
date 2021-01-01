6e61ba932c69d7ea4551adf3688bc9cec189e0c7
Eric Hou <eric.hou@firstwavecloud.com>
2021-01-01 12:38:42 +1100

    Updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WebsitesAvailability',
-    use_scm_version={"write_to": "_version.py", "fallback_version": "1.0"},
+    version="v1.1.0",
     author='Liang Hou',
     author_email='eric.hou.liang@gmail.com',
     license='Apache 2.0',
